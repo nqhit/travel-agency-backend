1b4f6e7cb050c8b6212d4808cf1ea684080a7a06
nqhit <nguyenquochuy812003@gmail.com>
2025-03-10 22:34:12 +0700

    ...

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ const PORT = process.env.PORT || 5000; // Set the port from environment variable
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+module.exports = app;
