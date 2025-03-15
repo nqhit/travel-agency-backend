6363f77f574be57547ce345f9b462eab062b6a23
nqhit <nguyenquochuy812003@gmail.com>
2025-03-15 14:55:28 +0700

    2:55 PM

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const app = require("./src/app"); // Import the app from app.js
-import cors from 'cors';
-const PORT = process.env.PORT || 5000; // Set the port from environment variables or default to 5000
+const app = require("./src/app"); 
+const PORT = process.env.PORT || 5000;
 
-// Start the server
-app.use(cors());
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT} `);
+  console.log(`🚀 http://localhost:${PORT} `);
 });
 
 module.exports = app;
--- src/app.js
+++ src/app.js
@@ -10,33 +10,29 @@ const productRoutes = require("./routes/productRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 // const wishlistRoutes = require("./routes/wishlistRoutes");
 const logger = require("./config/logger");
+
 const { errorHandler } = require("./middlewares/errorMiddleware");
-
-// Load environment variables
-require("dotenv").config();
-
-// Connect to MongoDB
-connectDB();
-
+const corsOptions = {
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
 const app = express();
 
-// Middleware for logging requests
+require("dotenv").config();
+connectDB();
+
+
 app.use((req, res, next) => {
   logger.info(`${req.method} ${req.url}`);
   next();
 });
 
-// Middleware for parsing JSON requests
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Cho phép xử lý form data
-
-// CORS configuration
-const corsOptions = {
-  origin: "*", // Change this to your frontend URL in production
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization",
-};
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 
 // Define routes
 app.use("/auth", authRoutes);
@@ -48,13 +44,10 @@ app.use("/products", productRoutes);
 app.use("/categories", categoryRoutes);
 // app.use("/wishlist", wishlistRoutes);
 
-// Health check route
 app.get("/", (req, res) => {
   res.send("JinStore API is running...");
 });
 
-// Error handling middleware
 app.use(errorHandler);
 
-// Export the app
 module.exports = app;
